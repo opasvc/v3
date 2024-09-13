2e7da6e711dfb864c80976f8f0312ab401d95be9
å <dztz663@gmail.com>
2024-09-13 09:05:22 +0800

    初始化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import ELP from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$axios = axios
+app.use(router)
+app.use(ELP)
+app.mount('#app')
