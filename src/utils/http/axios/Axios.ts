e3aeb28139294df7edb067f91da1cf6248d4f264
å <dztz663@gmail.com>
2024-09-14 10:11:29 +0800

    微调

--- src/utils/http/axios/Axios.ts
+++ src/utils/http/axios/Axios.ts
@@ -1,37 +1,132 @@
-import axios, { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import axios, { type AxiosRequestConfig } from 'axios'
 
-// 创建axios请求实例
-const service: AxiosInstance = axios.create({
-  baseURL: 'http://local:33590',
-  timeout: 5000 // 请求超时时间
-})
+axios.defaults.baseURL = localStorage.getItem('BASE_URL')?.toString()
+axios.defaults.timeout = 20 * 1000
+axios.defaults.maxBodyLength = 5 * 1024 * 1024
+axios.defaults.withCredentials = true
 
-// 请求拦截器
-service.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    // 在发送请求之前做些什么，比如添加token
-    const token = localStorage.getItem('token')
-    if (token && config.headers) {
-      config.headers.Authorization = `Bearer ${token}`
+axios.interceptors.request.use(
+  (config: AxiosRequestConfig | any) => {
+    config.params = {
+      ...config.params,
+      t: Date.now()
     }
     return config
   },
-  (error) => {
-    // 对请求错误做些什么
+  function (error) {
     return Promise.reject(error)
   }
 )
 
-// 响应拦截器
-service.interceptors.response.use(
-  (response: AxiosResponse) => {
-    // 对响应数据做些什么
-    return response.data
+// 添加响应拦截器
+axios.interceptors.response.use(
+  (response) => {
+    return response
   },
-  (error) => {
-    // 对响应错误做些什么
+  function (error) {
     return Promise.reject(error)
   }
 )
 
-export default service
+interface ResType<T> {
+  code: number
+  data?: T
+  msg: string
+  err?: string
+}
+
+interface Http {
+  get<T>(url: string, params?: unknown): Promise<T>
+
+  post<T>(url: string, params?: unknown): Promise<T>
+
+  upload<T>(url: string, params: unknown): Promise<T>
+
+  put<T>(url: string, params: unknown): Promise<T>
+
+  delete<T>(url: string, params: unknown): Promise<T>
+
+  download(url: string): void
+}
+
+const http: Http = {
+  get (url, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url, { params })
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  post (url, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(url, JSON.stringify(params))
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  put (url, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .put(url, JSON.stringify(params))
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  delete (url, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .delete(url, { params })
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  upload (url, file) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(url, file, {
+          headers: { 'Content-Type': 'multipart/form-data' }
+        })
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  download (url) {
+    const iframe = document.createElement('iframe')
+    iframe.style.display = 'none'
+    iframe.src = url
+    iframe.onload = function () {
+      document.body.removeChild(iframe)
+    }
+
+    document.body.appendChild(iframe)
+  }
+}
+
+export default http
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,41 +1,29 @@
 <template>
   <div class="relative w-full h-full px-4">
-    <el-form
-    ref="ruleFormRef"
-    style="max-width: 600px"
-    :model="ruleForm"
-    status-icon
-    :rules="rules"
-    label-width="auto"
-    class="demo-ruleForm"
-  >
-    <el-form-item label="Password" prop="pass">
-      <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
-    </el-form-item>
-    <el-form-item label="Confirm" prop="checkPass">
-      <el-input
-        v-model="ruleForm.checkPass"
-        type="password"
-        autocomplete="off"
-      />
-    </el-form-item>
-    <el-form-item label="Age" prop="age">
-      <el-input v-model.number="ruleForm.age" />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm(ruleFormRef)">
-        Submit
-      </el-button>
-      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
-    </el-form-item>
-  </el-form>
+    <el-form ref="ruleFormRef" style="max-width: 600px" :model="ruleForm" status-icon :rules="rules" label-width="auto"
+      class="demo-ruleForm">
+      <el-form-item label="Password" prop="pass">
+        <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Confirm" prop="checkPass">
+        <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Age" prop="age">
+        <el-input v-model.number="ruleForm.age" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(ruleFormRef)"> Submit </el-button>
+        <el-button type="error" @click="login"> Submit </el-button>
+        <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
 import type { FormInstance, FormRules } from 'element-plus'
-
+// import { axios } from '../utils/http/axios/Axios'
 const ruleFormRef = ref<FormInstance>()
 
 const ruleForm = reactive({
@@ -63,6 +51,9 @@ const resetForm = (formEl: FormInstance | undefined) => {
   formEl.resetFields()
 }
 
+const login = () => {
+  console.log('asd')
+}
 </script>
 
 <style lang="less"></style>
